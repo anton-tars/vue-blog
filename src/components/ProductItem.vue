b8e073a1b2c6280baa6d4cd8f4ef37ac50f368eb
Anton Stepanov <astepanov@sfbtech.org>
2023-05-18 20:30:39 +0300

    add products

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const props = defineProps(['product'])
+</script>
+
+<template>
+    <li class="product" >
+        <span class="image">
+            <img :src=props.product.image width="20" />
+        </span>
+        <label>{{ props.product.title }}</label>
+    </li>
+</template>
+
+<style scoped>
+    .image {
+        margin: 5px;
+    }
+</style>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ProductItem from '@/components/ProductItem.vue'
+import { ref } from 'vue';
+
+const productsList = ref({});
+
+async function getData() {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const finalRes = await res.json();
+    productsList.value = finalRes;
+}
+
+getData()
+</script>
+
+<template>
+    <ul class="products-list">
+        <product-item
+            v-for="product in productsList"
+            :product="product"
+        ></product-item>
+    </ul>
+</template>
